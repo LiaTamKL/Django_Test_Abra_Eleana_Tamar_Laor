24d75100dabd530ef96ad33b439f49d4e94979b8
LiaTamKL <ellaor6@gmail.com>
2022-06-24 21:29:13 +0300

    fixed static error

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -3,13 +3,14 @@ import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-620w%=z3%fmo5vl=)bof1ot6+otsvtids=1sh0#vxt0i)+)uv2' #os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'eleana-abra-test-message-app.herokuapp.com' ]
 
 STATIC_HOST = "https://d4663kmspf1sqa.cloudfront.net" 
+#STATIC_URL = STATIC_HOST + "/static/"
 
 DATABASES = {
     'default': {
